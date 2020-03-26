a770b3b4f8b8bed70a408480c6097645f52b6923
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-26 14:12:35 -0700

    add && and || condition

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
     var gender = $("select#gender").val();
 
     var quote = (100 - age) * 3;
-    
+    if (gender === 'male' && age < 26) {
+      quote += 50;
+    }
+
     $("#rate").empty().append(quote);
     $("#quote").show();
 
